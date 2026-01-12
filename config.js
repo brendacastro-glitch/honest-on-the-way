1453077a3673387544add1d0cdbfb7ca2ba55085
brendacastro-glitch <brendacastro@honestimmigration.com>
2026-01-12 13:56:46 -0500

    Update APP_CONFIG with new table definitions
    Added new tables to APP_CONFIG for client management.

--- config.js
+++ config.js
@@ -1,6 +1,14 @@
 window.APP_CONFIG = {
   supabase: {
     url: "https://qdvhobijcfbhnfzxpvlq.supabase.co",
-    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkdmhvYmlqY2ZiaG5menhwdmxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc2OTU0MzUsImV4cCI6MjA4MzI3MTQzNX0.qZA2_a7PdVpRxJJLoG5UKnnL3POcAT4Zy7BsPmCwvLc"
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkdmhvYmlqY2ZiaG5menhwdmxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc2OTU0MzUsImV4cCI6MjA4MzI3MTQzNX0.qZA2_a7PdVpRxJJLoG5UKnnL3POcAT4Zy7BsPmCwvLc",
+    tables: {
+      case_logins: 'case_logins',
+      clients: 'clients',
+      cases: 'cases',
+      documents: 'client_documents',     // You need to create this
+      tasks: 'client_tasks',            // You need to create this
+      updates: 'case_updates'           // You need to create this
+    }
   }
 };
